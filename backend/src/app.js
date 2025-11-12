f490e5d39b5d15a788655029bf369476067b3c8a
rafin0906 <rafrafin13579@gmail.com>
2025-11-12 07:36:39 +0600

    chore: initialize backend project with package.json and dependencies

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,11 @@
 import express from "express"
 import cors from "cors"
+import path from "path";
+
 
 const app = express()
+
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -18,6 +22,12 @@ app.use(express.static("public"))
 
 
 app.use("/api/v1/", chatRouter)
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('/', (_, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+});
+
 
 export { app }
 
